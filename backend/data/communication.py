54088eed4d4ffe72ddb0c97739368716594a849b
furqan <furqan@xaver.com>
2025-03-20 15:20:20 +0100

    changes

---
+++ backend/data/communication.py
@@ -0,0 +1,6 @@
+# get prompts
+# get login details for email address
+# get chat history for email address
+# update chat history for email address
+# get learned data for email address
+# updated learned data for email address
